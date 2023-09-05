d65e3581c74f14234244e7b47599991c1b2a628b
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-09-05 16:51:36 +0100

    added react to weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoader(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "f8e14a21ac2a08b12d64743366f61697";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Type in a city" onChange={changeCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loader) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Navigation">
+      <header>
+        <ul className="navigation-items">
+          <li className="navigation-item">Lisbon</li>
+          <li className="navigation-item">Paris</li>
+          <li className="navigation-item">Sydney</li>
+          <li className="navigation-item">San Francisco</li>
+        </ul>
       </header>
     </div>
   );
 }
-
-export default App;
+export default function App() {
+  return (
+    <form>
+      <input type="search" placeholder="Enter a City....." />
+      <button type="Submit">Search</button>
+      <button type="Submit">Current</button>
+    </form>
+  );
+}
