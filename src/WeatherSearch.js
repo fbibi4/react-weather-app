1660291b57d76a065c28601415c9dca7c4adea05
fbibi4 <127233513+fbibi4@users.noreply.github.com>
2023-09-10 17:56:54 +0100

    added a 5 day weather forecast

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -5,6 +5,7 @@ export default function WeatherSearch() {
   const [city, setCity] = useState("");
   const [loader, setLoader] = useState(false);
   const [weather, setWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
   const [cityName, setCityName] = useState("");
 
   useEffect(() => {
@@ -12,9 +13,11 @@ export default function WeatherSearch() {
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+      const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+      const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-      axios.get(apiUrl).then(displayWeather);
+      axios.get(weatherApiUrl).then(displayWeather);
+      axios.get(forecastApiUrl).then(displayForecast);
     });
   }, []);
 
@@ -30,11 +33,28 @@ export default function WeatherSearch() {
     });
   }
 
+  function displayForecast(response) {
+    const forecastData = response.data.list;
+    const uniqueDays = [];
+    const filteredForecast = forecastData.filter((item) => {
+      const dayOfWeek = getDayOfWeek(item.dt);
+      if (!uniqueDays.includes(dayOfWeek)) {
+        uniqueDays.push(dayOfWeek);
+        return true;
+      }
+      return false;
+    });
+    setForecast(filteredForecast.slice(0, 5)); // Get the first 5 unique days of forecast data
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     const apiKey = "f8e14a21ac2a08b12d64743366f61697";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    setForecast([]);
+    axios.get(weatherApiUrl).then(displayWeather);
+    axios.get(forecastApiUrl).then(displayForecast);
   }
 
   function changeCity(event) {
@@ -46,10 +66,29 @@ export default function WeatherSearch() {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
       const apiKey = "f8e14a21ac2a08b12d64743366f61697";
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-      axios.get(apiUrl).then(displayWeather);
+      const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+      const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+      setCityName("");
+
+      axios.get(weatherApiUrl).then(displayWeather);
+      axios.get(forecastApiUrl).then(displayForecast);
     });
   }
+
+  const getDayOfWeek = (timestamp) => {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const date = new Date(timestamp * 1000);
+    return daysOfWeek[date.getDay()];
+  };
 
   let form = (
     <form onSubmit={handleSubmit}>
@@ -86,6 +125,21 @@ export default function WeatherSearch() {
             <img src={weather.icon} alt={weather.description} />
           </li>
         </ul>
+        <h3>5-Day Forecast:</h3>
+        <div className="forecast">
+          {forecast.map((forecastItem, index) => (
+            <div className="forecast-item" key={index}>
+              <div className="day">{getDayOfWeek(forecastItem.dt)}</div>
+              <img
+                src={`http://openweathermap.org/img/wn/${forecastItem.weather[0].icon}@2x.png`}
+                alt={forecastItem.weather[0].description}
+              />
+              <div className="temperature">
+                {Math.round(forecastItem.main.temp)}°C
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   } else {
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
-import Navigation from "./Navigation";
 import WeatherSearch from "./WeatherSearch";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="container">
       <div className="App">
-        <Navigation />
         <WeatherSearch />
         <App />
       </div>
